3307bda96bc3167f44ba61ed1d3d82e52f7a99b2
Natthapong Thanessakul <natthapong.bsn@gmail.com>
2023-12-06 14:31:03 +0700

    สร้าง 2 url

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/register', function (req, res) {
+    res.send('Register Page')
+  })
+
+app.listen(3000)
